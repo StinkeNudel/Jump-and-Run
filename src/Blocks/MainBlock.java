0b7e9c03875c52ccb8b0913ed26bda5edfe0f9bd
StinkeNudel <henrikstarke@yahoo.de>
2019-11-04 17:39:31 +0100

    halp

---
+++ src/Blocks/MainBlock.java
@@ -0,0 +1,10 @@
+package Blocks;
+
+import Entity.Entity;
+
+public abstract class MainBlock extends Entity {
+
+    public MainBlock(double x, double y) {
+        super(x, y);
+    }
+}
--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -9,6 +9,7 @@ import java.awt.image.BufferedImage;
 public class Player extends Entity {
     BufferedImage image = ImageLoader.loadImage("/test.jpg");
     private Game game;
+    private int playerWidth = 100, playerHeight = 100;
 
     public Player(Game game, double x, double y) {
         super(x, y);
@@ -17,11 +18,26 @@ public class Player extends Entity {
 
     @Override
     public void tick() {
-
+        input();
     }
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(image, (int) x, (int) y, null);
+        g.drawImage(image, (int) x, (int) y, playerWidth, playerHeight, null);
+    }
+
+    private void input() {
+        if (game.getKeyHandler().w) {
+            y = y - 3;
+        }
+        if (game.getKeyHandler().a) {
+            x = x - 3;
+        }
+        if (game.getKeyHandler().s) {
+            y = y + 3;
+        }
+        if (game.getKeyHandler().d) {
+            x = x + 3;
+        }
     }
 }
