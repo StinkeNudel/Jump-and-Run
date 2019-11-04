556a2c16eecd7736d3493a110323addf52d6dbf4
jgehle <judige@freenet.de>
2019-11-04 17:12:05 +0100

    switch the asshole

--- src/Entity/Letter.java
+++ src/Entity/Letter.java
@@ -1,14 +1,19 @@
 package Entity;
 
 import GFX.ImageLoader;
+import Main.Game;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Letter extends Entity {
     private BufferedImage image; //= ImageLoader.loadImage("");
-    public Letter(double x, double y) {
+
+    private Game game;
+
+    public Letter(Game game, double x, double y) {
         super(x, y);
+        this.game = game;
     }
 
     public void tick() {
@@ -20,6 +25,30 @@ public class Letter extends Entity {
     }
 
     public void move() {
+        int howToFly = 0;
+        switch (howToFly) {
+            case 0:
+                x = x - 1;
+                if (x == game.width - 50) {
+                    howToFly++;
+                }
+                break;
+            case 1:
+                x = x - 1;
+                y = y - 1;
+                if (x == game.width - 55 && y == game.height - 250) {
+                    howToFly++;
+                }
+                break;
+            case 2:
+                x = x + 1;
+                y = y - 1;
+                if (x == game.width - 50 && y == game.height - 255) {
+                    howToFly++;
+                }
+                break;
+
+        }
 
     }
 }
