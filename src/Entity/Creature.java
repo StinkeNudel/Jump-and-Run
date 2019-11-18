a8c58ad0b6348fe064b7f4285922fc2b7278f2d2
StinkeNudel <henrikstarke@yahoo.de>
2019-11-18 19:26:12 +0100

    ok

--- src/Entity/Creature.java
+++
@@ -1,9 +0,0 @@
-package Entity;
-
-public abstract class Creature extends Entity {
-    int health;
-    public Creature(double x, double y) {
-        super(x, y);
-        int health = 1;
-    }
-}
