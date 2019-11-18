507661f31dbeab732c3554f9f88f7d07952daad7
jgehle <judige@freenet.de>
2019-11-18 18:10:15 +0100

    enemy

---
+++ src/Entity/Enemy.java
@@ -0,0 +1,25 @@
+package Entity;
+
+import java.awt.*;
+
+public class Enemy extends Creature {
+    public Enemy(double x, double y) {
+        super(x, y);
+
+        health = 2;
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+
+    public void walk() {
+
+    }
+}
