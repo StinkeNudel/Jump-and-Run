3ded0ce21afac65922675e4429da7d2e6b4caa34
jgehle <judige@freenet.de>
2019-11-18 18:19:45 +0100

    safh

--- src/Entity/Enemy.java
+++ src/Entity/Enemy.java
@@ -1,8 +1,17 @@
 package Entity;
 
+import Blocks.SolidBlocks;
+import Main.ArrayLists;
+
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Enemy extends Creature {
+
+    int enemyWidth = 60;
+    int enemyHeight = 60;
+    boolean notfalling = false;
+
     public Enemy(double x, double y) {
         super(x, y);
 
@@ -21,5 +30,22 @@ public class Enemy extends Creature {
 
     public void walk() {
 
+    }
+
+    public void checkBlocks() {
+        double BlockX, BlockY;
+        ArrayList solidBlocks = ArrayLists.getSolidBlocks();
+        for (int w = 0; w < solidBlocks.size(); w++) {
+            SolidBlocks m = (SolidBlocks) solidBlocks.get(w);
+            BlockX = m.getX();
+            BlockY = m.getY();
+
+            if (y + enemyHeight >= BlockY - 1 && ((BlockX > x && BlockX < x + enemyWidth))) {
+                notfalling = true;
+                return;
+            } else {
+                notfalling = false;
+            }
+        }
     }
 }
