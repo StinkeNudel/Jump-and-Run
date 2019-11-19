216b31247a01320ec67da6758b2ea1d5b49d0290
jgehle <judige@freenet.de>
2019-11-19 17:04:05 +0100

    hat der einfach meinen Enemy weggemacht

---
+++ src/Entity/Enemy.java
@@ -0,0 +1,19 @@
+package Entity;
+
+import java.awt.*;
+
+public class Enemy extends Creature {
+    public Enemy(double x, double y) {
+        super(x, y);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+}
