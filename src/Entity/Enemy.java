aa5466e51409567ddff72afd2f60c6f1ec337afb
LasagnenBrot <judige@freenet.de>
2019-11-20 21:29:09 +0100

    better worm animation

--- src/Entity/Enemy.java
+++ src/Entity/Enemy.java
@@ -157,6 +157,38 @@ public class Enemy extends Creature {
                 break;
             case 9:
                 image = ImageLoader.loadImage("/worm10.png");
+                animation++;
+                break;
+            case 10:
+                image = ImageLoader.loadImage("/worm9.png");
+                animation++;
+                break;
+            case 11:
+                image = ImageLoader.loadImage("/worm8.png");
+                animation++;
+                break;
+            case 12:
+                image = ImageLoader.loadImage("/worm7.png");
+                animation++;
+                break;
+            case 13:
+                image = ImageLoader.loadImage("/worm6.png");
+                animation++;
+                break;
+            case 14:
+                image = ImageLoader.loadImage("/worm5.png");
+                animation++;
+                break;
+            case 15:
+                image = ImageLoader.loadImage("/worm4.png");
+                animation++;
+                break;
+            case 16:
+                image = ImageLoader.loadImage("/worm3.png");
+                animation++;
+                break;
+            case 17:
+                image = ImageLoader.loadImage("/worm2.png");
                 animation = 0;
                 break;
 
