e25c727d0cb7dcf15c4fd4150958063dba717e45
jgehle <judige@freenet.de>
2019-11-22 18:48:27 +0100

    you can die and there is a wormywormywormy

---
+++ src/Worlds/Defeat.java
@@ -0,0 +1,21 @@
+package Worlds;
+
+import Main.Game;
+
+import java.awt.*;
+
+public class Defeat extends World {
+    public Defeat(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+}
